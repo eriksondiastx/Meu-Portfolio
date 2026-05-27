c2eb9973a2141ed8c6dff6169dab23d05591f744
Erikson Teixeira <67543510+eriksondiastx@users.noreply.github.com>
2026-05-27 09:30:13 +0100

    sync: actualizar data.js [2026-05-27T08:30:08.871Z]

--- js/data.js
+++ js/data.js
@@ -257,6 +257,28 @@ const defaultData = {
         "imagem/trabalhos/StAndrews5.jpg",
         "imagem/trabalhos/StAndrews6.jpg"
       ]
+    },
+    {
+      "periodo": "2020 - 2022",
+      "cargo": "Professor de Informática",
+      "empresa": "ATL StAndrews English School",
+      "localizacao": "Luanda, Angola",
+      "responsabilidades": [
+        "Lecionar a disciplina de Informática para crianças e adolescentes",
+        "Lecionar a disciplina de Artes Plásticas",
+        "Auxíliar os alunos na resolução das tarefas escolares",
+        "Preparação de material didático e avaliações",
+        "Acompanhamento do progresso dos alunos",
+        "Ensino de pacote Office (Word, Excel, PowerPoint)",
+        "Criação de actividades para datas comemorativas",
+        "Auxílio ao coordenador na gestão de disciplinas e professores",
+        "Suporte técnico das TICs do centro"
+      ],
+      "logo": "imagem/uploads/1779870597103_StAndrews.jpg",
+      "imagens": [],
+      "legendas": [
+        ""
+      ]
     }
   ],
   "projetos": [
@@ -364,7 +386,7 @@ const defaultData = {
       "description": "Especialização em hooks, context API e gerenciamento de estado global com Redux."
     }
   ],
-  "lastUpdated": 1779814493402
+  "lastUpdated": 1779870608871
 };
 
 window.defaultData = defaultData;
